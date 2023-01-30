b5379d35ac588cdfb2bf77306ec121c21745916e
Macintosh <rassadinmisha714@gmail.com>
2023-01-30 17:35:49 +0300

    домашка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car[] cars = new Car[5];
+        cars[0] = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
+        cars[1] = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+        cars[2] = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+        cars[3] = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        cars[4] = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+
+        listOfAllCar(cars);
+
+        Car car6 = new Car(null, null, 0, null, 0, null);
+        System.out.println(car6);
+    }
+    public static void listOfAllCar(Car[] cars) {
+        int emp = 0;
+        for (int i = 0; i < cars.length; i++) {
+            emp = emp + cars.length;
+            System.out.println(cars[i].toString());
+        }
     }
 }
