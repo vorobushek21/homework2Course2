7aab850607c0ea671344fabab30e85fe978349c3
Macintosh <rassadinmisha714@gmail.com>
2023-02-01 23:16:50 +0300

    домашка

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
+import transport.Car;
 public class Main {
     public static void main(String[] args) {
-        Car[] cars = new Car[5];
-        cars[0] = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
-        cars[1] = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
-        cars[2] = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
-        cars[3] = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
-        cars[4] = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        Car car1 = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия",
+                "механика", "седан", "т333тт92", 5, true, null);
+        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия", "автомат",
+                "универсал", "у999уу99", 0, true, new Car.Key(false,true));
+        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия",
+                "механика", "родстер", "к888кк88", 2, false, null);
+        Car car4 = new Car("KIA", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея",
+                "автомат", "внедорожник", "т444тт44", 5, true, new Car.Key(true,true));
+        Car car5 = new Car("Hyundai", "Avante", 1.6, " оранжевый", 2016, "Южная Корея",
+                "автомат", "седан", "н555нн66", 5, true, new Car.Key(true,false));
 
-        listOfAllCar(cars);
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+        System.out.println(car4);
+        System.out.println(car5);
 
-        Car car6 = new Car(null, null, 0, null, 0, null);
+        Car car6 = new Car("BMW", "5", 3.0, "красный", 2022, "Германия",
+                "автомат", "седан", null, 5, false, null) ;
+
+        car6.changeTires(5);
         System.out.println(car6);
-    }
-    public static void listOfAllCar(Car[] cars) {
-        int emp = 0;
-        for (int i = 0; i < cars.length; i++) {
-            emp = emp + cars.length;
-            System.out.println(cars[i].toString());
-        }
     }
 }
